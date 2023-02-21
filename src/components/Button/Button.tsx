e6f953766dea27dea991e7aa08d0e42c4b7f43ad
Eklal Budhathoki <budhathoki1418@gmail.com>
2023-02-22 00:27:59 +0545

    refact: Refact husky

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,7 +5,7 @@ export interface ButtonProps {
 }
 
 const Button = (props: ButtonProps) => {
-  return <button className="button h-10 w-10"> Hero of the year {props.label}</button>;
+  return <button className="button h-10 w-10">This is also changed Hero of the year {props.label}</button>;
 };
 
 export default Button;
