c8e46b8252404be7e9052a1ec59e774485f99b9e
Eklal Budhathoki <budhathoki1418@gmail.com>
2023-02-21 23:57:25 +0545

    add: Add eslint and prettier

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,25 +1,24 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
-import babel from '@rollup/plugin-babel';
-import terser from '@rollup/plugin-terser';
-import dts from 'rollup-plugin-dts';
-import postcss from 'rollup-plugin-postcss';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import babel from "@rollup/plugin-babel";
+import terser from "@rollup/plugin-terser";
+import dts from "rollup-plugin-dts";
+import postcss from "rollup-plugin-postcss";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
 export default [
   {
-    input: 'src/index.ts',
+    input: "src/index.ts",
     output: [
       {
-        file: 'dist/cjs/index.js',
-        format: 'cjs',
+        file: "dist/cjs/index.js",
+        format: "cjs",
         sourcemap: true,
       },
       {
-        file: 'dist/esm/index.js',
-        format: 'esm',
+        file: "dist/esm/index.js",
+        format: "esm",
         sourcemap: true,
       },
     ],
@@ -27,27 +26,27 @@ export default [
       peerDepsExternal(),
       resolve(),
       commonjs(),
-      typescript({ tsconfig: './tsconfig.json' }),
+      typescript({ tsconfig: "./tsconfig.json" }),
       postcss({
         config: {
-          path: './postcss.config.js',
+          path: "./postcss.config.js",
         },
-        extensions: ['.css'],
+        extensions: [".css"],
         minimize: true,
         inject: {
-          insertAt: 'top',
+          insertAt: "top",
         },
       }),
       babel({
-        exclude: 'node_modules/**',
-        presets: ['@babel/preset-react'],
+        exclude: "node_modules/**",
+        presets: ["@babel/preset-react"],
       }),
       terser(),
     ],
   },
   {
-    input: 'dist/esm/types/index.d.ts',
-    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
     external: [/\.css$/],
   },
