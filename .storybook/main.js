d7dfe6dabe3fd6df6e75ac17daf600b932642a82
Eklal Budhathoki <budhathoki1418@gmail.com>
2023-02-21 21:59:38 +0545

    feat: setup tailwind css

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,34 @@
+const path = require('path');
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
-}
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5',
+  },
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.css$/i,
+      use: [
+        {
+          loader: 'postcss-loader',
+          options: { implementation: require.resolve('postcss') },
+        },
+      ],
+      include: path.resolve(__dirname, './.storybook/'),
+    });
+    return config;
+  },
+};
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+
+module.exports = {
+    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {
+        spacing: {
+          '128': '32rem'
+        }      
+      },
+    },
+    variants: {
+      extend: {
+      },
+    },
+    plugins: [
+    ],
+  }
